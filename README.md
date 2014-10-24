4ba396ea2b2833d64653baa43d4684e1d8bd1af5
Dominik Zogg <dominik.zogg@gmail.com>
2014-10-24 20:23:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# saxulum-http-client-adapter-joomla
+
+[![Build Status](https://api.travis-ci.org/saxulum/saxulum-http-client-adapter-joomla.png?branch=master)](https://travis-ci.org/saxulum/saxulum-http-client-adapter-joomla)
+[![Total Downloads](https://poser.pugx.org/saxulum/saxulum-http-client-adapter-joomla/downloads.png)](https://packagist.org/packages/saxulum/saxulum-http-client-adapter-joomla)
+[![Latest Stable Version](https://poser.pugx.org/saxulum/saxulum-http-client-adapter-joomla/v/stable.png)](https://packagist.org/packages/saxulum/saxulum-http-client-adapter-joomla)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/saxulum/saxulum-http-client-adapter-joomla/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/saxulum/saxulum-http-client-adapter-joomla/?branch=master)
+
+## Features
+
+ * Provides a http client interface adapter for [joomla][1]
+
+## Requirements
+
+ * PHP 5.3+
+ * Joomla Http ~1.1
+
+## Installation
+
+Through [Composer](http://getcomposer.org) as [saxulum/saxulum-http-client-adapter-joomla][2].
+
+## Usage
+
+``` {.php}
+use Saxulum\HttpClient\Joomla\HttpClient;
+use Saxulum\HttpClient\Request;
+
+$httpClient = new HttpClient();
+$response = $httpClient->request(new Request(
+    '1.1',
+    Request::METHOD_GET,
+    'http://en.wikipedia.org',
+    array(
+        'Connection' => 'close',
+    )
+));
+```
+
+You can inject your own joomla browser instance while creating the http client instance.
+
+``` {.php}
+use Joomla\Http\Http;
+use Joomla\Http\Transport\Stream as StreamTransport;
+use Saxulum\HttpClient\Joomla\HttpClient;
+
+
+$options = array();
+$transport = new StreamTransport($options);
+$http = new Http($options, $transport);
+$httpClient = new HttpClient($http);
+```
+
+[1]: https://packagist.org/packages/joomla/http
+[2]: https://packagist.org/packages/saxulum/saxulum-http-client-adapter-joomla
---
+++ src/HttpClient.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Saxulum\HttpClient\Joomla;
+
+use Joomla\Http\HttpFactory;
+use Joomla\Http\Http;
+use Joomla\Http\Response as JoomlaResponse;
+use Saxulum\HttpClient\HttpClientInterface;
+use Saxulum\HttpClient\Request;
+use Saxulum\HttpClient\Response;
+
+class HttpClient implements HttpClientInterface
+{
+    /**
+     * @var Http
+     */
+    protected $http;
+
+    /**
+     * @param Http $http
+     */
+    public function __construct(Http $http = null)
+    {
+        $this->http = null !== $http ? $http : HttpFactory::getHttp();
+    }
+
+    /**
+     * @param  Request    $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function request(Request $request)
+    {
+        $methodName = strtolower($request->getMethod());
+
+        if (!is_callable(array($this, $methodName))) {
+            throw new \Exception("Unsupported method '{$request->getMethod()}'!");
+        }
+
+        /** @var JoomlaResponse $joomlaResponse */
+        $joomlaResponse = $this->$methodName($request);
+
+        return new Response(
+            (string) $request->getProtocolVersion(),
+            $joomlaResponse->code,
+            self::getStatusMessage($joomlaResponse->code),
+            $joomlaResponse->headers,
+            $joomlaResponse->body
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function options(Request $request)
+    {
+        return $this->http->options(
+            (string) $request->getUrl(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function get(Request $request)
+    {
+        return $this->http->get(
+            (string) $request->getUrl(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function head(Request $request)
+    {
+        return $this->http->head(
+            (string) $request->getUrl(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function post(Request $request)
+    {
+        return $this->http->post(
+            (string) $request->getUrl(),
+            $request->getContent(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function put(Request $request)
+    {
+        return $this->http->put(
+            (string) $request->getUrl(),
+            $request->getContent(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function delete(Request $request)
+    {
+        return $this->http->delete(
+            (string) $request->getUrl(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param  Request        $request
+     * @return JoomlaResponse
+     */
+    protected function path(Request $request)
+    {
+        return $this->http->patch(
+            (string) $request->getUrl(),
+            $request->getHeaders()
+        );
+    }
+
+    /**
+     * @param $statusCode
+     * @return string
+     * @throws \Exception
+     */
+    protected static function getStatusMessage($statusCode)
+    {
+        static $reflectionHttpClientInterface;
+
+        if (null === $reflectionHttpClientInterface) {
+            $httpClientInterface = 'Saxulum\HttpClient\HttpClientInterface';
+            $reflectionHttpClientInterface = new \ReflectionClass($httpClientInterface);
+        }
+
+        $constantName = self::getCodeConstantName($statusCode);
+
+        if (!$reflectionHttpClientInterface->hasConstant($constantName)) {
+            throw new \Exception("Unknown status code {$statusCode}!");
+        }
+
+        return $reflectionHttpClientInterface->getConstant($constantName);
+    }
+
+    /**
+     * @param $statusCode
+     * @return string
+     */
+    protected static function getCodeConstantName($statusCode)
+    {
+        return 'CODE_' . $statusCode;
+    }
+}
